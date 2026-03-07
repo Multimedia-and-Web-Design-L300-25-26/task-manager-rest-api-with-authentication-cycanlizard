27e5f5dfc7be399d04a307d0e455ad39624870a8
Edmond <ekdare@st.ug.edu.gh>
2026-03-07 17:40:50 -0600

    Implement task management functions in taskController

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -1,1 +1,23 @@
+exports.getTasks = (req, res) => {
+  res.status(200).json({
+    message: "Tasks retrieved successfully",
+    tasks: []
+  });
+};
 
+exports.createTask = (req, res) => {
+  const { title } = req.body;
+
+  res.status(201).json({
+    message: "Task created successfully",
+    task: { title }
+  });
+};
+
+exports.deleteTask = (req, res) => {
+  const { id } = req.params;
+
+  res.status(200).json({
+    message: `Task ${id} deleted successfully`
+  });
+};
