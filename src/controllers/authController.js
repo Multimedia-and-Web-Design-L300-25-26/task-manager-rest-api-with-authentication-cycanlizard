b1edda86909f6e7b1178273db6770fe93f12feb0
Edmond <ekdare@st.ug.edu.gh>
2026-03-07 17:35:45 -0600

    Implement user registration and login endpoints

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,1 +1,17 @@
+exports.register = (req, res) => {
+  const { email, password } = req.body;
 
+  res.status(201).json({
+    message: "User registered successfully",
+    user: { email }
+  });
+};
+
+exports.login = (req, res) => {
+  const { email, password } = req.body;
+
+  res.status(200).json({
+    message: "Login successful",
+    token: "fake-jwt-token"
+  });
+};
